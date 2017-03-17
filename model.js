2b4ebccb8af5ccf078bb3d430cb1b147ca01cc36
mike <mike@dotnetbox>
2017-03-17 11:51:22 +0000

    Updated to pull from uk crime data

--- model.js
+++ model.js
@@ -13,8 +13,10 @@ function Model (koop) {}
 // This is the only public function you need to implement
 Model.prototype.getData = function (req, callback) {
   // Call the remote API with our developer key
-  const key = config.trimet.key
-  request(`https://developer.trimet.org/ws/v2/vehicles/onRouteOnly/false/appid/${key}`, (err, res, body) => {
+  const date = config.crime.date
+  const lat = config.crime.lat
+  const lon = config.crime.lon
+  request(`http://data.police.uk/api/crimes-street/all-crime?lat=${lat}&lng=${lon}&date=${date}`, (err, res, body) => {
     if (err) return callback(err)
     // translate the response into geojson
     const geojson = translate(body)
@@ -26,24 +28,26 @@ Model.prototype.getData = function (req, callback) {
 }
 
 function translate (input) {
+  // console.log(input)
   return {
     type: 'FeatureCollection',
-    features: input.resultSet.vehicle.map(formatFeature)
+    features: input.map(formatFeature)
   }
 }
 
-function formatFeature (vehicle) {
+function formatFeature (crime) {
   // Most of what we need to do here is extract the longitude and latitude
   const feature = {
     type: 'Feature',
-    properties: vehicle,
+    properties: crime,
     geometry: {
       type: 'Point',
-      coordinates: [vehicle.longitude, vehicle.latitude]
+      coordinates: [Number(crime.location.longitude), Number(crime.location.latitude)]
     }
   }
+
   // But we also want to translate a few of the date fields so they are easier to use downstream
-  const dateFields = ['expires', 'serviceDate', 'time']
+  const dateFields = ['month']
   dateFields.forEach(field => {
     feature.properties[field] = new Date(feature.properties[field]).toISOString()
   })
@@ -53,40 +57,24 @@ function formatFeature (vehicle) {
 module.exports = Model
 
 /* Example raw API response
-{
-  "resultSet": {
-  "queryTime": 1488465776220,
-  "vehicle": [
+[
     {
-      "expires": 1488466246000,
-      "signMessage": "Red Line to Beaverton",
-      "serviceDate": 1488441600000,
-      "loadPercentage": null,
-      "latitude": 45.5873117,
-      "nextStopSeq": 1,
-      "source": "tab",
-      "type": "rail",
-      "blockID": 9045,
-      "signMessageLong": "MAX  Red Line to City Center & Beaverton",
-      "lastLocID": 10579,
-      "nextLocID": 10579,
-      "locationInScheduleDay": 24150,
-      "newTrip": false,
-      "longitude": -122.5927705,
-      "direction": 1,
-      "inCongestion": null,
-      "routeNumber": 90,
-      "bearing": 145,
-      "garage": "ELMO",
-      "tripID": "7144393",
-      "delay": -16,
-      "extraBlockID": null,
-      "messageCode": 929,
-      "lastStopSeq": 26,
-      "vehicleID": 102,
-      "time": 1488465767051,
-      "offRoute": false
+        "category": "anti-social-behaviour",
+        "location_type": "Force",
+        "location": {
+            "latitude": "50.917743",
+            "street": {
+                "id": 775315,
+                "name": "On or near Supermarket"
+            },
+            "longitude": "-1.486118"
+        },
+        "context": "",
+        "outcome_status": null,
+        "persistent_id": "9a27c321edf4272818bfcb4a6770816b0f39cfc54ae1182fd074cd324de9065f",
+        "id": 53951475,
+        "location_subtype": "",
+        "month": "2016-12"
     }
-  ]
-}
+]
 */
--- test/model-test.js
+++ test/model-test.js
@@ -11,8 +11,8 @@ const model = new Model()
 const nock = require('nock')
 
 test('should properly fetch from the API and translate features', t => {
-  nock('https://developer.trimet.org')
-  .get('/ws/v2/vehicles/onRouteOnly/false/appid/8A0EBB788E8205888807BAC97')
+  nock('http://data.police.uk')
+  .get('/api/crimes-street/all-crime?lat=50.91877&lng=-1.49037&date=2016-12')
   .reply(200, require('./fixtures/input.json'))
 
   model.getData({}, (err, geojson) => {
@@ -22,11 +22,9 @@ test('should properly fetch from the API and translate features', t => {
     const feature = geojson.features[0]
     t.equal(feature.type, 'Feature', 'has proper type')
     t.equal(feature.geometry.type, 'Point', 'creates point geometry')
-    t.deepEqual(feature.geometry.coordinates, [-122.675109, 45.5003833], 'translates geometry correctly')
+    t.deepEqual(feature.geometry.coordinates, [-1.486118, 50.917743], 'translates geometry correctly')
     t.ok(feature.properties, 'creates attributes')
-    t.equal(feature.properties.expires, new Date(1484268019000).toISOString(), 'translates expires field correctly')
-    t.equal(feature.properties.expires, new Date(1484268019000).toISOString(), 'translates serviceDate field correctly')
-    t.equal(feature.properties.expires, new Date(1484268019000).toISOString(), 'translates time field correctly')
+    t.equal(feature.properties.month, new Date('2016-12').toISOString(), 'translates expires field correctly')
     t.end()
   })
 })
